6afb704f3c3ba5412d3a13c9c0c08cd9c4bd6065
Ammar Kazi <muhammadammarkazi@gmail.com>
2023-10-16 20:46:30 -0400

    Updated Cache and Loader

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from langchain.sql_database import SQLDatabase
 #from dotenv import load_dotenv
 #load_dotenv()
 from langchain.cache import SQLiteCache
+import csv
+
 
 #os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 #OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -19,6 +21,26 @@ db = SQLDatabase.from_uri(dburi)
 llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
 toolkit = SQLDatabaseToolkit(llm = llm, db = db)
 set_llm_cache(SQLiteCache(database_path=".cache.db"))
+
+###
+csv_file = "Seating Sample List.csv"
+names_array = []
+
+# Open the CSV file
+with open(csv_file, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    # Get the field names and remove leading/trailing whitespace
+    fieldnames = [field.strip() for field in reader.fieldnames]
+
+    # Iterate through each row in the CSV
+    for row in reader:
+        first_name = row[fieldnames[0]]
+        last_name = row[fieldnames[1]]
+        full_name = f"{first_name} {last_name}"
+
+        names_array.append(full_name)
+
 
 custom_suffix = """
 I am a SQL agent that queries the database to help users find their table number. I always have access to the database.
@@ -54,6 +76,8 @@ agent = create_sql_agent(
     toolkit = toolkit
 )
 
+#for name in names_array:
+#    agent.run(name)
 
 def set_bg_hack_url():
     '''
@@ -84,5 +108,8 @@ if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
         st_callback = StreamlitCallbackHandler(st.container())
-        response = agent.run(prompt, callbacks=[st_callback])
-        st.write(response)
+        with st.spinner('Finding Your Table...'):
+            response = agent.run(prompt)
+            st.write(response)
+        st.success('Thank You For Joining Us!')
+        st.balloons()
